fffffaf6ccc5a26f960a2bc6c81e23c03b8f2e18
sdefrancesco <skaterbastian@aol.com>
2025-04-14 10:06:03 -0400

    add view engine to express

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+// modules
 const express = require('express')
-
 const app = express()
+// express options
+app.set('views', './views')
+app.set('view engine', 'hbs')
+
+// routes
+app.get('/', (req, res)=> {
+    res.render('index.hbs')
+})
 
 
-
-
-
+// express server
 const PORT = 3000
 app.listen(PORT, () => {
     console.log(`server started on ${PORT}`)
