1d25bfeb7b99634bb644db6168a80c9329855575
sdefrancesco <skaterbastian@aol.com>
2025-04-14 20:56:28 -0400

    add delete post delete request and filter query

--- data.js
+++ data.js
@@ -2,38 +2,48 @@ module.exports = {
 
     users : [
         {
+            id: 1,
             name: 'sebastian defrancesco',
             dob: '8/3/1996',
             email: 'sdefrancesco37@gmail.com'
         },
         {
-            name: 'sebastian defrancesco',
-            dob: '8/3/1996',
-            email: 'sdefrancesco37@gmail.com'
+            id: 2,
+            name: 'jonathan moy',
+            dob: '5/3/1996',
+            email: 'jonathanm@gmail.com'
         },
         {
-            name: 'sebastian defrancesco',
-            dob: '8/3/1996',
-            email: 'sdefrancesco37@gmail.com'
+            id: 3,
+            name: 'karen james',
+            dob: '2/3/1996',
+            email: 'karenj@gmail.com'
         },
         {
-            name: 'sebastian defrancesco',
-            dob: '8/3/1996',
-            email: 'sdefrancesco37@gmail.com'
+            id: 4,
+            name: 'jacob read',
+            dob: '12/3/1996',
+            email: 'jacobr@gmail.com'
         },
     ],
     
     posts : [
         {
+            id: 1,
             title: 'First Post',
+            date: new Date(),
             content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est a sint eligendi aliquid quibusdam expedita provident molestias suscipit dolorum dolorem!',
         },
         {
+            id: 2,
             title: 'Second Post',
+            date: new Date(),
             content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est a sint eligendi aliquid quibusdam expedita provident molestias suscipit dolorum dolorem!',
         },
         {
+            id: 3,
             title: 'Third Post',
+            date: new Date(),
             content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est a sint eligendi aliquid quibusdam expedita provident molestias suscipit dolorum dolorem!',
         },
     
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // modules
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 
 // data
 const data = require('./data')
@@ -10,19 +11,74 @@ const app = express()
 // express options
 app.set('views', './views')
 app.set('view engine', 'hbs')
+
 // serve static files in public directory like css and images
 app.use(express.static(path.join(__dirname, 'public')))
 
+// parse body data from form for post, and put requests
+app.use(bodyParser.urlencoded({extended: false}))
+
 // routes
 app.get('/', (req, res) => {
+    console.log(req.query);
+
+    let filteredPosts = data.posts
+
+    // search by title
+    if (req.query.search) {
+        const searchQuery = req.query.search.toLowerCase()
+        filteredPosts = filteredPosts.filter(post => 
+            post.title.toLowerCase().includes(searchQuery)
+        );
+    }
+
+    // date filter
+    if (req.query.date) {
+        if (req.query.date === 'mostRecent') {
+            // sort by most recent 
+            filteredPosts = filteredPosts.sort((a, b) => new Date(b.date) - new Date(a.date));
+        } else if (req.query.date === 'oldestToNewest') {
+            // sort by oldest to newest
+            filteredPosts = filteredPosts.sort((a, b) => new Date(a.date) - new Date(b.date));
+        }
+    }
+
+    // render the view with filtered posts
     res.render('index.hbs', {
         users: data.users,
-        posts: data.posts
-    })
+        posts: filteredPosts
+    });
+});
+
+
+app.post('/users/new', (req, res)=> {
+    console.log(req.body)
+    let newUser = { ...req.body, id : data.users.length + 1}
+    data.users.push(newUser)
+    res.redirect('/')
 })
 
-// 
-// app.post('/posts/new')
+app.post('/users/:id/delete', (req, res) => {
+    const postIndex = data.users.findIndex(users => users.id == req.params.id);
+    data.users.splice(postIndex, 1);
+    res.redirect('/')
+});
+
+
+app.post('/posts/new', (req, res)=> {
+    console.log(req.body)
+    let newPost = { ...req.body, date : new Date(), id : data.posts.length + 1}
+    data.posts.push(newPost)
+    res.redirect('/')
+})
+
+// DELETE REQUEST
+app.post('/posts/:id/delete', (req, res) => {
+    const postIndex = data.posts.findIndex(post => post.id == req.params.id);
+    data.posts.splice(postIndex, 1);
+    res.redirect('/')
+    
+});
 
 
 // express server
